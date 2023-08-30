c44b67aee93d23adc83833a110630c615ed77543
Pedro Gomes <up202108825@up.pt>
2023-08-30 19:30:02 +0100

    created user, weightlog and foodlog models

--- fithub/models.py
+++ fithub/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    age = models.IntegerField(default=0)
+    weight = models.FloatField(default=0)
+    height = models.FloatField(default=0)
+
+class WeightLog(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    weight = models.FloatField(default=0)
+    date = models.DateField(auto_now_add=True)
+
+class FoodLog(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    calories = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
