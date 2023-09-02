17d4d074f5c62b4a272f1d4fce4d90924b9b5f37
Pedro Gomes <up202108825@up.pt>
2023-09-02 22:20:51 +0100

    profile view animation

--- fithub/static/fithub/script.js
+++ fithub/static/fithub/script.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('#profile').addEventListener('click', function(){
-        window.location.href = '/profile'
+        document.querySelector('#profile-view').style.opacity = '100%';
+        document.querySelector('#profile-view').style.width = '40%';
+        document.querySelector('#profile-view').style.height = '75%';
+        document.querySelector('#food').style.opacity = '30%';
+        document.querySelector('#weight').style.opacity = '30%';
+    })
+    document.querySelector('#profile-view svg').addEventListener('click', function(){
+        document.querySelector('#profile-view').style.opacity = '0';
+        document.querySelector('#profile-view').style.width = '0';
+        document.querySelector('#profile-view').style.height = '0';
+        document.querySelector('#food').style.opacity = '100%';
+        document.querySelector('#weight').style.opacity = '100%';
     })
 })
