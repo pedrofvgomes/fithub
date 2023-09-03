d632dfb5143e613a848048ee8b29aa37effe8a38
Pedro Gomes <up202108825@up.pt>
2023-09-03 01:48:57 +0100

    fixed models and views

--- fithub/models.py
+++ fithub/models.py
@@ -3,16 +3,25 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     age = models.IntegerField(default=0)
-    weight = models.FloatField(default=0)
+    starting_weight = models.FloatField(default=0)
+    current_weight = models.FloatField(default=0)
     height = models.FloatField(default=0)
+    gender = models.CharField(max_length=50)
 
 class WeightLog(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE)
-    weight = models.FloatField(default=0)
+    weight = models.FloatField()
     date = models.DateField(auto_now_add=True)
 
 class FoodLog(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    calories = models.IntegerField(default=0)
+    calories = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+class NutritionalInfo(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    objective = models.FloatField()
+    bmr = models.IntegerField()
+    activity = models.CharField(max_length=100)
+    caloric_need = models.IntegerField()
--- fithub/static/fithub/script.js
+++ fithub/static/fithub/script.js
@@ -5,8 +5,14 @@ document.addEventListener('DOMContentLoaded', function(){
         document.querySelector('#profile-view').style.height = '75%';
         document.querySelector('#food').style.opacity = '30%';
         document.querySelector('#weight').style.opacity = '30%';
+        document.querySelectorAll('#profile-view > *').forEach( element => {
+            element.style.display = 'block';
+        });
     })
     document.querySelector('#profile-view svg').addEventListener('click', function(){
+        document.querySelectorAll('#profile-view > *').forEach( element => {
+            element.style.display = 'none';
+        });
         document.querySelector('#profile-view').style.opacity = '0';
         document.querySelector('#profile-view').style.width = '0';
         document.querySelector('#profile-view').style.height = '0';
